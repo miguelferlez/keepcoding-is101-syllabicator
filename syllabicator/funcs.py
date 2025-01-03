e37996f93bca2df32c1f8d02e7940d85a06b506a
Miguel Fernandez <miguelferlez@gmail.com>
2025-01-03 13:47:47 +0100

    fix: Split correction when word begins with prefix

--- syllabicator/funcs.py
+++ syllabicator/funcs.py
@@ -37,19 +37,34 @@ def split(word:str):
     prev_char = ''
     vowel_pos = []
     current_pos = 0
-    
+    was_triphthong = False
+    was_diphthong = False
+    was_prefix = False
+
     for index, char in enumerate(word.lower()):
         if char in VOWELS or char == 'y':
             if is_consonant_pair(prev_char, word[index-2]):
                 vowel_pos.append(index-2)
-            elif prev_char in CONSONANTS or prev_char == 'y':
+            elif (prev_char in CONSONANTS or prev_char == 'y') and not was_prefix:
                 vowel_pos.append(index-1)
             elif is_triphthong(char, prev_char, word[index-2]):
+                was_triphthong = True
                 vowel_pos.pop(-1)
             elif is_diphthong(char, prev_char) or word[-1] == 'y':
+                was_diphthong = True
                 vowel_pos.append(index+1)
             else:
                 vowel_pos.append(index)
+                was_prefix = False
+        else:
+            if char == 'n' or char == 's':
+                if index == len(word)-1 and not was_triphthong:
+                    vowel_pos.pop(-1)
+                if index == 1 and (prev_char in VOWELS or was_diphthong):
+                    was_prefix = True
+                    vowel_pos.append(index+1)
+            was_triphthong = False
+            was_diphthong = False    
         prev_char = char
 
     for pos in vowel_pos[1:]:
@@ -58,7 +73,7 @@ def split(word:str):
 
     result.append(word[current_pos:len(word)])
     result = list(filter(None, result))
-
+    # print(vowel_pos)
     print(result)
     return result
 
@@ -68,3 +83,5 @@ if __name__ == '__main__':
     split('cambiéis')
     split('miau')
     split('fluye')
+    split('cienpies')
+    split('yegua')
--- tests/test_syllabicator_funcs.py
+++ tests/test_syllabicator_funcs.py
@@ -3,6 +3,7 @@ from syllabicator.funcs import split
 def test_split():
     assert split('elefante') == ['e', 'le', 'fan', 'te']
     assert split('constante') == ['cons', 'tan', 'te']
+    assert split('inaccion') == ['in', 'ac', 'cion']
     
 def test_split_diphthong():
     assert split('caucho') == ['cau', 'cho']
@@ -10,12 +11,12 @@ def test_split_diphthong():
     assert split('iones') == ['io', 'nes']
 
     assert split('soy') == ['soy']
+    assert split('yegua') == ['ye', 'gua']
 
 def test_split_triphthong():
     assert split('paraguay') == ['pa', 'ra', 'guay']
     assert split('cambiéis') == ['cam', 'biéis']
     assert split('miau') == ['miau']
-    pass
 
 def test_split_consonant_pairs():
     assert split('carretera') == ['ca', 'rre', 'te', 'ra']
